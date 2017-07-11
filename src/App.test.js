426ba61a4d23a105cef59c337334388a83c889a8
MichaelCook <michael@evoluted.net>
2017-07-11 21:20:33 +0100

    Updated tests to use Enzyme to test rendered content

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
+/**
+ * Jira / enzyme unit tests for simple issues
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
+// Test that it renders the app
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+// Test if it displays simple html
+it('renders flat content', () => {
+  const typicalContent = <h1>Sky Betting &amp; Gaming Technical Test</h1>;
+  expect(shallow(<App />).contains(typicalContent)).toEqual(true);
 });
