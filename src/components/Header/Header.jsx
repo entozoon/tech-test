47e3ba08387a0c3878f8ea03c01bfd11e7df7d40
MichaelCook <michael@evoluted.net>
2017-07-12 01:24:59 +0100

    Adding in some quick header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,13 @@ import logo from './images/logo.png';
 const Header = () =>
   <header className="header">
     <div className="wrapper">
+      <div className="header__title">
+        <h1>Sky Betting &amp; Gaming Technical Test</h1>
+      </div>
+
       <a href="#" className="header__logo">
         <img src={logo} alt="SkyBet" />
       </a>
-      <h1>Sky Betting &amp; Gaming Technical Test</h1>
     </div>
   </header>;
 
